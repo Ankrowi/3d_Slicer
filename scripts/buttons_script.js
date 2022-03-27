90c6006563a0cc27acdac76ba91be5f7b8ffb8c2
Андрей Выдря <65971311+Ankrowi@users.noreply.github.com>
2022-03-28 00:51:43 +0300

    fifinal chandes

---
+++ scripts/buttons_script.js
@@ -0,0 +1,24 @@
+var main_picture_src = new Map()
+main_picture_src.set("view_full", "../pictures/Main_full.svg")
+main_picture_src.set("view_front", "../pictures/Main_front.svg")
+main_picture_src.set("view_top", "../pictures/Main_top.svg")
+main_picture_src.set("view_left", "../pictures/Main_left.svg")
+main_picture_src.set("view_right", "../pictures/Main_right.svg")
+
+function view_button_click(view_button) {
+    document.getElementById("main_picture").src = main_picture_src.get(view_button)
+    if (view_button !== "view_full") {
+        document.getElementById("main_picture").style.left = 500
+        document.getElementById("main_picture").style.top = 200
+    } else {
+        document.getElementById("main_picture").style.left = 700
+        document.getElementById("main_picture").style.top = 360
+    }
+}
+
+let view_buttons = document.getElementsByClassName("view_button")
+Array.from(view_buttons).forEach(function (button) {
+    button.addEventListener('click', function(e) {
+        view_button_click(e.target.id)
+    })
+})
