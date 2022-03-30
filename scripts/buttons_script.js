216e1c21e0f53a0640f13f9ccda033e3ec650676
Андрей Выдря <65971311+Ankrowi@users.noreply.github.com>
2022-03-30 22:39:41 +0300

    a

--- scripts/buttons_script.js
+++ scripts/buttons_script.js
@@ -5,6 +5,9 @@ main_picture_src.set("view_top", "../pictures/Main_top.svg")
 main_picture_src.set("view_left", "../pictures/Main_left.svg")
 main_picture_src.set("view_right", "../pictures/Main_right.svg")
 
+var buttons_ways = new Map()
+buttons_ways.set("custom_button", "../pictures/default_button.svg")
+
 function view_button_click(view_button) {
     document.getElementById("main_picture").src = main_picture_src.get(view_button)
     if (view_button !== "view_full") {
@@ -21,4 +24,13 @@ Array.from(view_buttons).forEach(function (button) {
     button.addEventListener('click', function(e) {
         view_button_click(e.target.id)
     })
+})
+
+function set_buttons_back_image(button, button_class) {
+    button.style.backgroundImage = "url(" + buttons_ways.get(button_class) + ")"
+}
+
+let def_buttons = document.getElementsByClassName("custom_button")
+Array.from(def_buttons).forEach(function(button) {
+    set_buttons_back_image(button, "custom_button")
 })
